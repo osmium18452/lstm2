683373ad3c16cd131d9bbb766e1f59b23a796bdd
osmium <1711042219@qq.com>
2020-05-04 22:21:41 +0800

    done

--- postprocess/train.py
+++ postprocess/train.py
@@ -3,30 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 a=[]
-for i in ('0.5','0.7','0.9','1.0'):
-	with open("../save/0505/dropout/"+i+"/train_ac.pkl","rb") as f:
+list=('0.1','0.01','0.001','0.0001','0.02','0.002','0.0002','0.05','0.005','0.0005')
+# list=('32','64','96','128')
+# list=('64','96','128','256')
+# list=('2','3','4','5','6')
+for i in list:
+	#---
+	with open("../save/0606/lr/"+i+"/train_ac.pkl","rb") as f:
 		a.append(pkl.load(f))
 
 plt.figure()
-ax1 = plt.subplot()
-plt.title("loss and accuracy of training and validating")
-ax1.set_xlabel("epochs")
-x = range(len(train_ac))
-ax1.set_ylabel("loss")
-ax2 = ax1.twinx()
-ax2.set_ylabel("accuracy")
-
+x=range(len(a[0]))
+# plt.xticks(x,lr)
+plt.xlabel("epochs")
+plt.ylabel("Final Accuracy (%)")
+# ---
+plt.title("Final Accuracy with Different Learning Rate")
 kwargs = {
-	"marker": None,
-	"lw": 2,
-}
-l1, = ax1.plot(x, train_ls, color="tab:blue", label="train loss", **kwargs)
-l3, = ax1.plot(x, valid_ls, color="tab:green", label="test loss", **kwargs)
-l4, = ax2.plot(x, train_ac, color="tab:red", label="test accuracy", **kwargs)
+			"marker": None,
+			"lw":2
+		}
 
-plt.legend(handles=[l1, l3, l4], loc="center right")
+for i,y in enumerate(a):
+	# ---
+	plt.plot(x, y, label="lr="+list[i], **kwargs)
+
+plt.legend()
 sv = plt.gcf()
-sv.savefig(os.path.join(args.save_dir, "lossAndAcc" + str(args.lr) +".png"), format="png", dpi=300)
-
-for y in a:
-
+# ---
+sv.savefig("../save/pic/lr.png", format="png", dpi=100)
+plt.show()
